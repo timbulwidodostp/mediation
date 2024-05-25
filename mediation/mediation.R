39d9f99f9a80d49c12b540b048c4ef023b4b1a80
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-25 13:31:11 +0700

    Use mediate (mediation) (In) With R Software

---
+++ mediation/mediation.R
@@ -0,0 +1,19 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Estimate sensitivity analysis for causal mediation effects Use mediate (mediation) (In) With R Software
+install.packages("mediation")
+install.packages("tibble")
+library("mediation")
+mediation = read.csv("https://raw.githubusercontent.com/timbulwidodostp/mediation/main/mediation/mediation.csv",sep = ";")
+# Estimate sensitivity analysis for causal mediation effects Use mediate (mediation) (In) With R Software
+mediation = read.csv("C:\\mediation.csv",sep = ";")
+mediation_1 <- lm(bweight ~ college + age, data=mediation)
+mediation_2 <- lm(sespar ~ college + bweight + age, data=mediation)
+mediation <- mediate(mediation_1, mediation_2, sims=1000, treat="college", mediator="bweight")
+summary(mediation)
+# Estimate sensitivity analysis for causal mediation effects Use mediate (mediation) (In) With R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
